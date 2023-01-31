c5af0b5894f41d0dbd964a4bd4a90c095633fd9e
ElectronSpark <820461770@qq.com>
2023-02-01 01:04:40 +0800

    almost get prepared for chroot environment

--- scripts/prepare_chroot.sh
+++ scripts/prepare_chroot.sh
@@ -160,3 +160,339 @@ rm -v ${LFS}/usr/lib/lib{stdc++,stdc++fs,supc++}.la
 
 popd
 
+
+# We've completed building cross-toolchain (pass1). The next step is to use
+# this toolchain to compile basic utilities and install them into their final
+# location. These basic utilities will be used after entering the chroot 
+# environment.
+
+
+# m4
+tar xf ${LFS_HOME}/sources/m4-1.4.19.tar.xz \
+    -C ${LFS_HOME}/build/m4                 \
+    --strip-components 1
+
+pushd ${LFS_HOME}/build/m4
+
+${LFS_HOME}/build/m4/configure      \
+    --prefix=/usr                   \
+    --host=${LFS_TGT}               \
+    --build=$(build-aux/config.guess)
+
+make
+make DESTDIR=${LFS} install
+
+popd
+
+
+# ncurses
+mkdir -pv ${LFS_HOME}/{build,sources}/ncurses
+tar -xf ${LFS_HOME}/sources/ncurses-6.3.tar.gz  \
+    -C ${LFS_HOME}/sources/ncurses              \
+    --strip-components 1
+
+pushd ${LFS_HOME}/build/ncurses
+
+# make sure that gawk is found first during configuration.
+sed -i s/mawk// ${LFS_HOME}/sources/ncurses/configure
+# build "tic" program on the build host. This can run on the building machine,
+# and it is needed to create the terminal database without error during
+# ncurses' installation.
+mkdir -pv build_tic
+pushd build_tic
+    ${LFS_HOME}/sources/ncurses/configure
+    make -C include
+    make -C progs tic
+popd
+
+# configure ncurses for compilation
+${LFS_HOME}/sources/ncurses/configure                   \
+    --prefix=/usr                                       \
+    --host=${LFS_TGT}                                   \
+    --build=$(${LFS_HOME}/sources/ncurses/config.guess) \
+    --mandir=/usr/share/man                             \
+    --with-manpage-format=normal                        \
+    --with-shared                                       \
+    --without-normal                                    \
+    --with-cxx-shared                                   \
+    --without-debug                                     \
+    --without-ada                                       \
+    --disable-stripping                                 \
+    --enable-widec
+
+make
+make DESTDIR=${LFS} TIC_PATH=$(pwd)/build_tic/progs/tic install
+echo "INPUT(-lncursesw)" > ${LFS}/usr/lib/libncurses.so
+popd
+
+
+# bash
+mkdir -pv ${LFS_HOME}/{build,sources}/bash
+tar -xf ${LFS_HOME}/sources/bash-5.1.16.tar.gz  \
+    -C ${LFS_HOME}/sources/bash                 \
+    --strip-components 1
+
+pushd ${LFS_HOME}/build/bash
+# bash's memory allocation function may cause segmentation faults
+${LFS_HOME}/sources/bash/configure                              \
+    --prefix=/usr                                               \
+    --host=${LFS_TGT}                                           \
+    --build=$(${LFS_HOME}/sources/bash/support/config.guess)    \
+    --without-bash-malloc
+
+make
+make DESTDIR=${LFS} install
+
+# make the bash just built as the default shell
+ln -sv bash ${LFS}/bin/sh
+popd
+
+
+# coreutils
+mkdir -pv ${LFS_HOME}/{build,sources}/coreutils
+tar -xf ${LFS_HOME}/sources/coreutils-9.1.tar.xz    \
+    -C ${LFS_HOME}/sources/coreutils                \
+    --strip-components 1
+
+pushd ${LFS_HOME}/build/coreutils
+
+${LFS_HOME}/sources/coreutils/configure                             \
+    --prefix=/usr                                                   \
+    --host=${LFS_TGT}                                               \
+    --build=$(${LFS_HOME}/sources/coreutils/build-aux/config.guess) \
+    --enable-install-program=hostname                               \
+    --enable-no-install-program=kill,uptime
+
+make
+make DESTDIR=${LFS} install
+
+mv -v ${LFS}/usr/bin/chroot                 ${LFS}/usr/sbin
+mkdir -pv ${LFS}/usr/share/man/man8
+mv -v ${LFS}/usr/share/man/man1/chroot.1    ${LFS}/usr/share/man/man8/chroot.8
+sed -i 's/"1"/"8"/'                         ${LFS}/usr/share/man/man8/chroot.8
+
+popd
+
+
+# diffutils
+mkdir -pv ${LFS_HOME}/{build,sources}/diffutils
+tar -xf ${LFS_HOME}/sources/diffutils-3.8.tar.xz    \
+    -C ${LFS_HOME}/sources/diffutils                \
+    --strip-components 1
+
+pushd ${LFS_HOME}/build/diffutils
+
+${LFS_HOME}/sources/diffutils/configure --prefix=/usr --host=${LFS_TGT}
+
+make 
+make DESTDIR=${LFS} install
+
+popd
+
+
+# file
+mkdir -pv ${LFS_HOME}/{build,sources}/file/{,host_build}
+tar -xf ${LFS_HOME}/sources/file-5.42.tar.gz    \
+    -C ${LFS_HOME}/sources/file                 \
+    --strip-components 1
+
+# the file command on the build machine needs to be same version as the one 
+# to be built.
+pushd ${LFS_HOME}/build/file/host_build
+    ${LFS_HOME}/sources/file/configure  \
+        --disable-bzlib                 \
+        --disable-libseccomp            \
+        --disable-xzlib                 \
+        --disable-zlib
+    make
+popd
+
+# build the file command on target machine
+pushd ${LFS_HOME}/build/file
+
+${LFS_HOME}/sources/file/configure                      \
+    --prefix=/usr                                       \
+    --host=${LFS_TGT}                                   \
+    --build=$(${LFS_HOME}/sources/file/config.guess)
+
+make FILE_COMPILE=${LFS_HOME}/build/file/host_build/src/file
+make DESTDIR=${LFS} install
+# remove harmful libtool archive file
+rm -v ${LFS}/usr/lib/libmagic.la
+
+popd
+
+
+# findutils
+mkdir -pv ${LFS_HOME}/{build,sources}/findutils
+tar -xf ${LFS_HOME}/sources/findutils-4.9.0.tar.xz  \
+    -C ${LFS_HOME}/sources/findutils                \
+    --strip-components 1
+
+pushd ${LFS_HOME}/build/findutils
+
+${LFS_HOME}/sources/findutils/configure                             \
+    --prefix=/usr                                                   \
+    --host=${LFS_TGT}                                               \
+    --build=$(${LFS_HOME}/sources/findutils/build-aux/config.guess) \
+    --localstatedir=/var/lib/locate
+
+make
+make DESTDIR=${LFS} install
+
+popd
+
+
+# gawk
+mkdir -pv ${LFS_HOME}/{build,sources}/gawk
+tar -xf ${LFS_HOME}/sources/gawk-5.1.1.tar.xz   \
+    -C ${LFS_HOME}/sources/gawk                 \
+    --strip-components 1
+
+pushd ${LFS_HOME}/build/gawk
+
+${LFS_HOME}/sources/gawk/configure                              \
+    --prefix=/usr                                               \
+    --host=${LFS_TGT}                                           \
+    --build=$(${LFS_HOME}/sources/gawk/build-aux/config.guess)
+
+make
+make DESTDIR=${LFS} install
+
+popd
+
+
+# grep
+mkdir -pv ${LFS_HOME}/{build,sources}/grep
+tar -xf ${LFS_HOME}/sources/grep-3.7.tar.xz \
+    -C ${LFS_HOME}/sources/grep             \
+    --strip-components 1
+
+pushd ${LFS_HOME}/build/grep
+
+${LFS_HOME}/sources/grep/configure                              \
+    --prefix=/usr                                               \
+    --host=${LFS_TGT}
+
+make
+make DESTDIR=${LFS} install
+
+popd
+
+
+# gzip
+mkdir -pv ${LFS_HOME}/{build,sources}/gzip
+tar -xf ${LFS_HOME}/sources/gzip-1.12.tar.xz    \
+    -C ${LFS_HOME}/sources/gzip                 \
+    --strip-components 1
+
+pushd ${LFS_HOME}/build/gzip
+
+${LFS_HOME}/sources/gzip/configure                              \
+    --prefix=/usr                                               \
+    --host=${LFS_TGT}
+
+make
+make DESTDIR=${LFS} install
+
+popd
+
+
+# make
+mkdir -pv ${LFS_HOME}/{build,sources}/make
+tar -xf ${LFS_HOME}/sources/make-4.3.tar.gz \
+    -C ${LFS_HOME}/sources/make             \
+    --strip-components 1
+
+pushd ${LFS_HOME}/build/make
+# adding option "--without-guile" is to prevent configure from using guile
+# from the building machine, which makes compilation fail.
+${LFS_HOME}/sources/make/configure                              \
+    --prefix=/usr                                               \
+    --host=${LFS_TGT}                                           \
+    --without-guile                                             \
+    --build=$(${LFS_HOME}/sources/make/build-aux/config.guess)
+
+make
+make DESTDIR=${LFS} install
+
+popd
+
+
+# patch
+mkdir -pv ${LFS_HOME}/{build,sources}/patch
+tar -xf ${LFS_HOME}/sources/patch-2.7.6.tar.xz  \
+    -C ${LFS_HOME}/sources/patch                \
+    --strip-components 1
+
+pushd ${LFS_HOME}/build/patch
+
+${LFS_HOME}/sources/patch/configure                             \
+    --prefix=/usr                                               \
+    --host=${LFS_TGT}                                           \
+    --build=$(${LFS_HOME}/sources/patch/build-aux/config.guess)
+
+make
+make DESTDIR=${LFS} install
+
+popd
+
+
+# sed
+mkdir -pv ${LFS_HOME}/{build,sources}/sed
+tar -xf ${LFS_HOME}/sources/sed-4.8.tar.xz  \
+    -C ${LFS_HOME}/sources/sed              \
+    --strip-components 1
+
+pushd ${LFS_HOME}/build/sed
+
+${LFS_HOME}/sources/sed/configure   \
+    --prefix=/usr                   \
+    --host=${LFS_TGT}                                           
+
+make
+make DESTDIR=${LFS} install
+
+popd
+
+
+# tar
+mkdir -pv ${LFS_HOME}/{build,sources}/tar
+tar -xf ${LFS_HOME}/sources/tar-1.34.tar.xz     \
+    -C ${LFS_HOME}/sources/tar                  \
+    --strip-components 1
+
+pushd ${LFS_HOME}/build/tar
+
+${LFS_HOME}/sources/tar/configure                               \
+    --prefix=/usr                                               \
+    --host=${LFS_TGT}                                           \
+    --build=$(${LFS_HOME}/sources/tar/build-aux/config.guess)
+
+make
+make DESTDIR=${LFS} install
+
+popd
+
+
+# xz
+mkdir -pv ${LFS_HOME}/{build,sources}/xz
+tar -xf ${LFS_HOME}/sources/xz-5.2.6.tar.xz     \
+    -C ${LFS_HOME}/sources/xz                   \
+    --strip-components 1
+
+pushd ${LFS_HOME}/build/xz
+
+${LFS_HOME}/sources/xz/configure                                \
+    --prefix=/usr                                               \
+    --host=${LFS_TGT}                                           \
+    --build=$(${LFS_HOME}/sources/xz/build-aux/config.guess)    \
+    --disable-static                                            \
+    --docdir=/usr/share/doc/xz-5.2.6
+
+make
+make DESTDIR=${LFS} install
+# remove harmful libtool archive file
+rm -v ${LFS}/usr/lib/liblzma.la
+
+popd

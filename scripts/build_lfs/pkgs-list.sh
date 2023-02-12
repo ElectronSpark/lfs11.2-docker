35e79f81c311738c174b8fb1e7c0a27e1c4fa128
ElectronSpark <820461770@qq.com>
2023-02-12 14:58:18 +0800

    add a script to automatically build all the tools under chroot, and rename some mistypes name

--- scripts/build_lfs/pkgs-list.sh
+++ scripts/build_lfs/pkgs-list.sh
@@ -1,76 +1,75 @@
-LFS_PKGS_NAME_LIST= \
-    man-pages       \
-    iana-etc        \
-    glibc           \
-    zlib            \
-    bzip2           \
-    xz              \
-    zstd            \
-    file            \
-    readline        \
-    m4              \
-    bc              \
-    flex            \
-    tcl             \
-    expect          \
-    dejagnu         \
-    binutils        \
-    gmp             \
-    mpfr            \
-    mpc             \
-    attr            \
-    acl             \
-    libcap          \
-    shadow          \
-    gcc             \
-    pkg-config      \
-    ncurses         \
-    sed             \
-    psmisc          \
-    gettext         \
-    bison           \
-    grep            \
-    bash            \
-    libtool         \
-    gdbm            \
-    gperf           \
-    expat           \
-    inetutils       \
-    less            \
-    perl            \
-    XML-Parser      \
-    intltool        \
-    autoconf        \
-    automake        \
-    openssl         \
-    kmod            \
-    libelf          \
-    libffi          \
-    python          \
-    wheel           \
-    ninja           \
-    meson           \
-    coreutils       \
-    check           \
-    diffutils       \
-    gawk            \
-    findutils       \
-    groff           \
-    grub            \
-    gzip            \
-    iproute2        \
-    kbd             \
-    libpipeline     \
-    make            \
-    patch           \
-    tar             \
-    texinfo         \
-    vim             \
-    markupsafe      \
-    jinja           \
-    systemd         \
-    dbus            \
-    man-db          \
-    procps-ng       \
-    util-linux      \
-    e2fsprogs
+man-pages
+iana-etc
+glibc
+zlib
+bzip2
+xz
+zstd
+file
+readline
+m4
+bc
+flex
+tcl
+expect
+dejagnu
+binutils
+gmp
+mpfr
+mpc
+attr
+acl
+libcap
+shadow
+gcc
+pkg-config
+ncurses
+sed
+psmisc
+gettext
+bison
+grep
+bash
+libtool
+gdbm
+gperf
+expat
+inetutils
+less
+perl
+XML-Parser
+intltool
+autoconf
+automake
+openssl
+kmod
+libelf
+libffi
+python3
+wheel
+ninja
+meson
+coreutils
+check
+diffutils
+gawk
+findutils
+groff
+grub
+gzip
+iproute2
+kdb
+libpipeline
+make
+patch
+tar
+texinfo
+vim
+markupsafe
+jinja
+systemd
+dbus
+man-db
+procps-ng
+util-linux
+e2fsprogs
